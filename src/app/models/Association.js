d40d5a2a90680ec697c1a0505c0a9d8fce2d8013
Tiago Cintra <tiagosilvacintra@yahoo.com.br>
2022-02-06 12:03:18 -0300

    Adição de unique key na tabela de Associações

--- src/app/models/Association.js
+++ src/app/models/Association.js
@@ -5,43 +5,53 @@ const City = require('../models/City');
 const Player = require('../models/Player');
 const Edition = require('../models/Edition');
 
-const Association = sequelize.define('association', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  playerId: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-    references: {
-      model: Player,
-      key: 'id',
+const Association = sequelize.define(
+  'association',
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    playerId: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: Player,
+        key: 'id',
+      },
+    },
+    cityId: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: City,
+        key: 'id',
+      },
+    },
+    initialEditionId: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: Edition,
+        key: 'id',
+      },
+    },
+    finalEditionId: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: Edition,
+        key: 'id',
+      },
     },
   },
-  cityId: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-    references: {
-      model: City,
-      key: 'id',
+  {
+    uniqueKeys: {
+      actions_unique: {
+        fields: ['playerId', 'cityId', 'initialEditionId'],
+      },
     },
   },
-  initialEditionId: {
-    type: Sequelize.INTEGER,
-    references: {
-      model: Edition,
-      key: 'id',
-    },
-  },
-  finalEditionId: {
-    type: Sequelize.INTEGER,
-    references: {
-      model: Edition,
-      key: 'id',
-    },
-  },
-});
+);
 
 module.exports = Association;
